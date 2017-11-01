b8c6145c43e5770084316ab5a78fab142ff79d95
Lisa Mae <lisamae135b7@me.com>
2017-11-01 11:54:49 -0400

    installed devise and bootstrap

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,8 @@
 class Company < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
 has_many :boats
 has_many :jobs
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :companies
+  root 'home#index'
   resources :boats
   resources :boat_jobs
   resources :customers
   resources :jobs
   resources :companies
   resources :posts
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
