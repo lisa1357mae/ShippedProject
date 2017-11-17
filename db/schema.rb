5ae68bdf5d98ec92761fa2ec1107f77ec2ec2c65
Lisa Mae <lisamae135b7@me.com>
2017-11-17 12:00:55 -0500

    fixed nav bar and login/logout issues

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 20171101154632) do
     t.string "username"
     t.string "firstname"
     t.string "lastname"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup). 
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Company.create ([{ username: 'jborbon',email: 'jborbon@live.com', firstname:'Joan', lastname:'Borbon', password: 'test123'},
+Company.create! ([{ username: 'jborbon',email: 'jborbon@live.com', firstname:'Joan', lastname:'Borbon', password: 'test123'},
 
   { username: 'lisamae',email: 'lisa@example.com', firstname: 'Lisa', lastname:
     'Mae', password: 'test123'}
